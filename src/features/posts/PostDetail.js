50fe0366d45afe1bcca348712d956d69a4f11cab
Aaron Dykes <aaronmdykes@gmail.com>
2025-06-25 22:43:26 -0400

    Refactor PostDetail component; enhance media rendering and styling for comments

--- src/features/posts/PostDetail.js
+++ src/features/posts/PostDetail.js
@@ -1,23 +1,18 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector as useGlobalSelector } from "react-redux";
 import { fetchComments } from "../comments/commentSlice";
 import CommentList from "../comments/CommentList";
 import "./PostDetail.css";
-
-import { useNavigate } from "react-router-dom";
-import { useSelector as useGlobalSelector } from "react-redux";
 
 export default function PostDetail() {
   const { id } = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  // Get post from posts state (could also fetch fresh if needed)
   const post = useGlobalSelector((state) =>
     state.posts.posts.find((p) => p.id === id || p.id === parseInt(id))
   );
-  const hasImage = post.image && /\.(jpg|jpeg|png|gif)$/i.test(post.image);
 
   useEffect(() => {
     if (post?.permalink) {
@@ -42,13 +37,35 @@ export default function PostDetail() {
 
       <div className="post-detail">
         <h2 className="post-detail-title">{post.title}</h2>
-        {hasImage && (
-          <img className="post-image" src={post.image} alt={post.title} />
+
+        {/* Conditionally render Reddit-hosted video */}
+        {post.post_hint === "hosted:video" &&
+          post.media?.reddit_video?.fallback_url && (
+            <video
+              className="post-detail-video"
+              controls
+              muted
+              src={post.media.reddit_video.fallback_url}
+            />
+          )}
+
+        {/* Conditionally render image */}
+        {post.post_hint === "image" && (
+          <img className="post-detail-image" src={post.url} alt={post.title} />
         )}
+
+        {/* Conditionally render selftext */}
+        {post.selftext && (
+          <div className="post-detail-body">
+            <p>{post.selftext}</p>
+          </div>
+        )}
+
         <p className="post-detail-author">Posted by u/{post.author}</p>
         <p className="post-detail-comments-count">
           {post.num_comments} comments
         </p>
+
         <hr />
         <CommentList />
       </div>
